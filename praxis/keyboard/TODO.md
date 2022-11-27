b5366b2d74c35d3ea933c2266a7414571ca53b9a
Guido Schmidt <git@guidoschmidt.cc>
2022-11-27 05:07:29 +0100

    FIX: proper delete of last <span class=text> element

---
+++ praxis/keyboard/TODO.md
@@ -0,0 +1,11 @@
+# 25.11.2022
+### Keyboard
+
+# TODO
+- Code ins Projekt kopieren (`keyboard` Ordner + `document.mjs` in javascript Ordner)
+- Was passiert in `index.mjs`, Ausprobieren + im Browser anschauen
+- document-API verwenden
+- switch Statement verwenden
+  - console.log() für key
+  - verschiedene "case" szenarios für verschiedene Keys
+  - "br" für Enter
--- praxis/keyboard/index.mjs
+++ praxis/keyboard/index.mjs
@@ -1,12 +1,42 @@
 import { runWhenKeyPressed } from "../../javascript/document.mjs";
 
 function reactOnKey(key) {
-  const element = document.createElement("span");
-  element.innerText = key;
-  document.body.insertBefore(element, document.querySelector(".cursor"));
-  // @TODO
-  // - console.log(key) verwenden um herauszufinden, was in "key" steht
-  // - switch cas verwenden um auf unterschiedliche Tasten zu reagieren
+
+  // querySelector sucht auf der HTML Seite nach einem Element
+  // mit der Klasse "cursor"
+  const cursor = document.querySelector(".cursor");
+
+  switch (key) {
+    case "Enter":
+      console.log(key, typeof key);
+      // 1. <br> Element erzeugen
+      // 2. Neue br element dem document hinzufügen
+      const brElement = document.createElement("br");
+      brElement.className = "text";
+      document.body.insertBefore(brElement, cursor);
+      break;
+
+
+    case "Backspace":
+      // Letzes Element auf der Seite löschen
+      // Was ist das letze element?
+      const textElementList = document.querySelectorAll(".text");
+      const lastElement = textElementList[textElementList.length - 1];
+      if (lastElement !== undefined) {
+        lastElement.remove()
+      }
+      break;
+
+
+    default:
+      // Erzeugt ein <span> Element
+      const element = document.createElement("span");
+      // Fügt den Wert aus "key" als Text dem <span> hinzu
+      element.className = "text";
+      element.innerText = key;
+      document.body.insertBefore(element, cursor);
+      break;
+  }
 }
 
 runWhenKeyPressed(reactOnKey);
