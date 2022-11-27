ff8549f4d3f8d2f8141bb5526e045c6b673f85d2
Guido Schmidt <git@guidoschmidt.cc>
2022-11-27 15:46:55 +0100

    ADD: object visualizer example

--- praxis/object-visualizer/index.mjs
+++ praxis/object-visualizer/index.mjs
@@ -1,32 +1,49 @@
 import { runWhenDocumentIsLoaded } from "../../javascript/document.mjs";
 // @TODO #1
 // visualizeObject aus "object.mjs" Datei importieren
+import { visualizeObject } from "../../javascript/object.mjs";
+
 
 runWhenDocumentIsLoaded(() => {
   // @TODO #2 Object "person" befüllen
-  let person = new Object();
-
+  let person = {
+    name: "Guido",
+    age: 33,
+    eyeColor: "green"
+  };
+  
   // @TODO #3
   // Objekt mit weiteren Einträgen befüllen
   // z.b. Alter, Größe, etc.
+  person["age"] = 34;
+  person.heightInMeters = 1.87;
+  person.name = "Tom";
+
 
   function getObjectItemWithKey(key) {
     // @TODO #3
     // Wert für den gegebenen "key" aus dem Objekt abfragen
     // und statt "undefined" zurück geben.
-    return undefined;
+    return person[key];
   }
 
 
   function removeObjectItemAtKey(key) {
     // @TODO #4
     // Eintrag für den gegebenen key aus dem Objekt löschen.
+    delete person[key];
   }
-  
+ 
+
   function replaceObjectItemAtKey(key, newValue) {
       // @TODO #5
       // Eintrag für den gegebenen key im Objekt
       // durch den neuen Wert "newValue" ersetzen.
+
+      // 1. console.log(person, key, newValue)
+      // 2. Ersetzen-Button klicken, was passiert?
+      // 3. Objekt person abändern mit dem key und auf "newValue" setzen
+      person[key] = newValue;
   }
 
   visualizeObject(
